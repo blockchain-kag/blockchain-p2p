314a12f72cff8300d4244e5b72486b7462f433da
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2026-03-25 12:06:47 -0300

    checkpoint:
    queda generalizar el comportamiento del calculate_hash por un lado.
    
    Por otra parte, estuve estructurando toda la logica del engine, pero quedan partes que faltan pulir.
    
    Estuve estructurando el codigo a grandes rasgos y estructure cierto comportamiento.

--- src/consensus_engine/validation/block_validator.rs
+++ src/consensus_engine/validation/block_validator.rs
@@ -9,7 +9,15 @@ impl BlockValidator {
     // fixme, no necesariamente tenemos que tener un previous
     // Que pasa si es el primer bloque?
     // fixme
-    pub fn validate(current: &Block, previous: &Block) -> bool {
+    pub fn validate(current: &Block, previous: Option<&Block>) -> bool {
+        if previous.is_none() {
+            return current.index == 0
+                && current.previous_hash.is_empty()
+                && current.hash == current.calculate_hash();
+        }
+
+        let previous = previous.unwrap();
+
         if current.hash != current.calculate_hash() { return false; }
         if current.previous_hash != previous.hash { return false; }
         if current.index != previous.index + 1 { return false; }
