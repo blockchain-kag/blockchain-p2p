6083f451525cc63d3da1cbad47ed920b994bb2f5
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2026-03-26 01:37:03 -0300

    Finished a better TCP Receiver implementation

--- src/network_layer_adapters/tcp/tcp_receiver.rs
+++ src/network_layer_adapters/tcp/tcp_receiver.rs
@@ -2,9 +2,10 @@ use std::{
     collections::{HashMap, VecDeque},
     io::{ErrorKind, Read},
     net::{SocketAddr, TcpListener, TcpStream},
+    ops::ControlFlow,
     sync::{
-        Arc, Mutex,
-        mpsc::{Receiver, channel},
+        Arc, Mutex, Weak,
+        mpsc::{Receiver, Sender, channel},
     },
     thread::{self, sleep},
     time::Duration,
@@ -28,53 +29,7 @@ impl TcpReceiver {
         let weak_peers = Arc::downgrade(&peers);
         let (tx, rx) = channel();
 
-        thread::spawn(move || {
-            loop {
-                let listener = match weak_listener.upgrade() {
-                    Some(l) => l,
-                    None => break,
-                };
-
-                match listener.accept() {
-                    Ok((stream, addr)) => {
-                        println!("New peer: {}", addr);
-                        if let Some(peers) = weak_peers.upgrade() {
-                            peers
-                                .lock()
-                                .unwrap()
-                                .insert(addr, stream.try_clone().unwrap());
-
-                            let tx_clone = tx.clone();
-                            thread::spawn(move || {
-                                let mut buffer = [0u8; 1024];
-                                let mut stream = stream;
-
-                                loop {
-                                    match stream.read(&mut buffer) {
-                                        Ok(0) => break,
-                                        Ok(n) => {
-                                            let msg =
-                                                String::from_utf8_lossy(&buffer[..n]).to_string();
-                                            tx_clone.send((addr, msg)).ok();
-                                        }
-                                        Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                                            sleep(Duration::from_millis(10));
-                                        }
-                                        Err(_) => break,
-                                    }
-                                }
-                            });
-                        } else {
-                            break;
-                        }
-                    }
-                    Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                        std::thread::sleep(Duration::from_millis(50));
-                    }
-                    Err(e) => panic!("{}", e),
-                }
-            }
-        });
+        spawn_new_peers_listener_thread(weak_listener, weak_peers, tx);
 
         Self {
             listener,
@@ -84,8 +39,67 @@ impl TcpReceiver {
     }
 }
 
+fn spawn_new_peers_listener_thread(
+    weak_listener: Weak<TcpListener>,
+    weak_peers: Weak<Mutex<HashMap<SocketAddr, TcpStream>>>,
+    tx: Sender<(SocketAddr, String)>,
+) {
+    thread::spawn(move || {
+        loop {
+            let listener = match weak_listener.upgrade() {
+                Some(l) => l,
+                None => break,
+            };
+
+            match listener.accept() {
+                Ok((stream, addr)) => {
+                    println!("New peer: {}", addr);
+                    if let Some(peers) = weak_peers.upgrade() {
+                        peers
+                            .lock()
+                            .unwrap()
+                            .insert(addr, stream.try_clone().unwrap());
+
+                        spawn_peer_reader_thread(stream, addr, tx.clone());
+                    } else {
+                        break;
+                    }
+                }
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                    sleep(Duration::from_millis(50));
+                }
+                Err(e) => panic!("{}", e),
+            }
+        }
+    });
+}
+
+fn spawn_peer_reader_thread(stream: TcpStream, addr: SocketAddr, tx: Sender<(SocketAddr, String)>) {
+    thread::spawn(move || {
+        let mut buffer = [0u8; 1024];
+        let mut stream = stream;
+
+        loop {
+            match stream.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(n) => {
+                    let msg = String::from_utf8_lossy(&buffer[..n]).to_string();
+                    tx.send((addr, msg)).ok();
+                }
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                    sleep(Duration::from_millis(10));
+                }
+                Err(_) => break,
+            }
+        }
+    });
+}
+
 impl NetworkReceiver for TcpReceiver {
     fn receive(&mut self) -> Option<(String, String)> {
-        None
+        match self.rx.try_recv() {
+            Ok((addr, msg)) => Some((addr.to_string(), msg)),
+            Err(_) => None,
+        }
     }
 }
