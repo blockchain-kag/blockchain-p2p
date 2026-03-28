1a9d6cbdaacd7d2e22984fb4d78621d3157dd693
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2026-03-28 02:15:43 -0300

    Made the cargo run

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-pub mod block;
 pub mod consensus_engine;
 pub mod mempool;
 pub mod network_layer;
 pub mod network_layer_adapters;
 pub mod node;
-pub mod transaction;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-use std::ptr::null;
-
-use blockchain_p2p::{
-    consensus_engine::types::engine::{self, Engine},
-    mempool::Mempool,
-    network_layer::{self, Network},
-    network_layer_adapters::tcp::{tcp_receiver::TcpReceiver, tcp_sender::TCPSender},
-    node::types::node::Node,
-};
-
 fn main() {
-    let receiver = TcpReceiver::new("0.0.0.0:9000");
-    let sender = TCPSender::new();
-    let network_layer = Network::new(Box::new(sender), Box::new(receiver));
-    let mempool = Mempool::new();
-    let node = Node::new(network_layer, mempool);
-    node.broadcast(String::from("s"));
+    println!("Starting node...")
 }
