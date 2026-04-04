3a39f6d7dbdbe6732356a0419e20263a9bc91d95
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2026-04-04 20:47:59 -0300

    Implemented prompt managing

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,86 @@ use std::{
 };
 
 use crossterm::{
-    QueueableCommand,
-    cursor::{self, MoveDown, MoveRight, MoveTo, MoveToColumn, MoveToRow},
-    execute, queue,
-    style::{self, Print, Stylize, style},
+    cursor::MoveTo,
+    event::{poll, read},
+    queue,
+    style::Print,
     terminal::{self, Clear, ClearType, WindowSize},
 };
 
 fn main() {
     let mut stdout = stdout();
     let w_size: WindowSize = terminal::window_size().unwrap();
-    loop {
+    let mut prompt = String::new();
+    terminal::enable_raw_mode().unwrap();
+    let mut quit = false;
+    while !quit {
+        while poll(Duration::ZERO).unwrap() {
+            match read().unwrap() {
+                crossterm::event::Event::FocusGained => todo!(),
+                crossterm::event::Event::FocusLost => todo!(),
+                crossterm::event::Event::Key(key_event) => match key_event.code {
+                    crossterm::event::KeyCode::Backspace => {
+                        prompt.pop();
+                    }
+                    crossterm::event::KeyCode::Enter => todo!(),
+                    crossterm::event::KeyCode::Left => todo!(),
+                    crossterm::event::KeyCode::Right => todo!(),
+                    crossterm::event::KeyCode::Up => todo!(),
+                    crossterm::event::KeyCode::Down => todo!(),
+                    crossterm::event::KeyCode::Home => todo!(),
+                    crossterm::event::KeyCode::End => todo!(),
+                    crossterm::event::KeyCode::PageUp => todo!(),
+                    crossterm::event::KeyCode::PageDown => todo!(),
+                    crossterm::event::KeyCode::Tab => todo!(),
+                    crossterm::event::KeyCode::BackTab => todo!(),
+                    crossterm::event::KeyCode::Delete => todo!(),
+                    crossterm::event::KeyCode::Insert => todo!(),
+                    crossterm::event::KeyCode::F(_) => todo!(),
+                    crossterm::event::KeyCode::Char(c) => {
+                        prompt.push(c);
+                    }
+                    crossterm::event::KeyCode::Null => todo!(),
+                    crossterm::event::KeyCode::Esc => {
+                        quit = true;
+                    }
+                    crossterm::event::KeyCode::CapsLock => todo!(),
+                    crossterm::event::KeyCode::ScrollLock => todo!(),
+                    crossterm::event::KeyCode::NumLock => todo!(),
+                    crossterm::event::KeyCode::PrintScreen => todo!(),
+                    crossterm::event::KeyCode::Pause => todo!(),
+                    crossterm::event::KeyCode::Menu => todo!(),
+                    crossterm::event::KeyCode::KeypadBegin => todo!(),
+                    crossterm::event::KeyCode::Media(_) => todo!(),
+                    crossterm::event::KeyCode::Modifier(_) => todo!(),
+                },
+                crossterm::event::Event::Mouse(_) => todo!(),
+                crossterm::event::Event::Paste(_) => todo!(),
+                crossterm::event::Event::Resize(_, _) => todo!(),
+            }
+        }
+
         queue!(stdout, Clear(ClearType::All)).unwrap();
         for y in 0..(w_size.rows - 2) {
             queue!(stdout, MoveTo(w_size.columns / 2, y), Print("║")).unwrap();
         }
         for x in 0..w_size.columns {
-            queue!(stdout, MoveTo(x, w_size.rows - 2), Print("═")).unwrap();
+            if x == w_size.columns / 2 {
+                queue!(
+                    stdout,
+                    MoveTo(w_size.columns / 2, w_size.rows - 2),
+                    Print("╩")
+                )
+                .unwrap();
+            } else {
+                queue!(stdout, MoveTo(x, w_size.rows - 2), Print("═")).unwrap();
+            }
         }
 
-        queue!(
-            stdout,
-            MoveTo(w_size.columns / 2, w_size.rows - 2),
-            Print("╩")
-        )
-        .unwrap();
+        queue!(stdout, MoveTo(0, w_size.rows), Print(&prompt)).unwrap();
 
         stdout.flush().unwrap();
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_millis(33));
     }
+    terminal::disable_raw_mode().unwrap();
 }
